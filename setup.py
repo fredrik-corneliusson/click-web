0dcc8dab4e90e73e50669b88ac0f1952b506ada1
Fredrik Corneliusson <fredrik.corneliusson@gmail.com>
2019-04-03 23:15:06 +0200

    Escape the list and link mouse over tips.

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ testing_requirements = [
 
 setup(
     name='click-web',
-    version='0.6.7',
+    version='0.6.8',
     url='https://github.com/fredrik-corneliusson/click-web',
     author='Fredrik Corneliusson',
     author_email='fredrik.corneliusson@gmail.com',

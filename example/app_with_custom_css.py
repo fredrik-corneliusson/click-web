800e84cc3ea6677c99ec7fab438a12b0e63d8aff
Fredrik Corneliusson <fredrik.corneliusson@gmail.com>
2019-04-10 22:58:29 +0200

    Made tree easier to style via CSS
    Added example of how to customize css.

--- example/app_with_custom_css.py
+++ example/app_with_custom_css.py
@@ -1,17 +1,20 @@
-from click_web import create_click_web_app
-from example import example_command
 from pathlib import Path
 
 from flask import Blueprint
 
-custom_folder = (Path(__file__).parent / 'custom').absolute()
-custom_folder_blueprint = Blueprint(
-    'custom',
-    __name__,
-    static_url_path='/custom',
-    static_folder=custom_folder)
+from click_web import create_click_web_app
+from example import example_command
 
+# create app as normal
 app = create_click_web_app(example_command, example_command.cli)
 
-app.config['CUSTOM_CSS'] = (custom_folder / 'custom.css').exists()
+# Expect any custom folder to be located in same folder as this script
+custom_folder = (Path(__file__).parent / 'custom').absolute()
+# Make custom folder reachable by "custom/static" url path (via a Flask blueprint)
+custom_folder_blueprint = Blueprint('custom', __name__,
+                                    static_url_path='/custom',
+                                    static_folder=custom_folder)
 app.register_blueprint(custom_folder_blueprint)
+
+# Set CUSTOM_CSS in flask config so the click-web will use it.
+app.config['CUSTOM_CSS'] = 'custom.css'

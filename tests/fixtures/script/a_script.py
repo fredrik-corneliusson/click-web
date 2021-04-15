c07068f4fa39e3900da6883f9a2ee7e85fe02700
Whatang <mike@whatang.org>
2021-04-15 23:36:38 +0100

    Use correct option command-line switch for options, not the option's parameter name.

--- tests/fixtures/script/a_script.py
+++ tests/fixtures/script/a_script.py
@@ -35,11 +35,11 @@ def a_sub_group_command():
 
 
 @cli.command()
-@click.option("--an-option", type=str, default="option_value", help='help for an option')
+@click.option("--an-option", "the_option_value", type=str, default="option_value", help='help for an option')
 @click.argument("an-argument", default=10, type=int)
-def command_with_option_and_argument(an_argument, an_option):
+def command_with_option_and_argument(an_argument, the_option_value):
     'Help text'
-    click.echo(f"Ran command with option: {an_option} argument {an_argument}")
+    click.echo(f"Ran command with option: {the_option_value} argument {an_argument}")
 
 
 @cli.command()

bfd7a0d4f0834f7ea5bb02c0c96a4e5ca06128be
Fredrik Corneliusson <fredrik.corneliusson@gmail.com>
2024-06-25 10:35:45 +0200

    Bumped version

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ dev_requirements = [
 
 setup(
     name='click-web',
-    version='0.8.5',
+    version='0.8.6',
     url='https://github.com/fredrik-corneliusson/click-web',
     author='Fredrik Corneliusson',
     author_email='fredrik.corneliusson@gmail.com',
